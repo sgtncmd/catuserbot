db03ea7836740ec4cef6711bea49d516b7f11952
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-26 21:16:27 +0530

    Update events.py

--- userbot/core/events.py
+++ userbot/core/events.py
@@ -268,28 +268,16 @@ async def send_file(
     safe_file_check = await safe_check_text(filemsg)
     if safecheck or safe_file_check:
         if Config.BOTLOG:
-            response = await client.sendfile(
+            response = await client.sendmessage(
                 entity=Config.BOTLOG_CHATID,
-                file=file,
-                caption=msg,
-                force_document=force_document,
-                file_size=file_size,
-                clear_draft=clear_draft,
-                progress_callback=progress_callback,
+                message=msg,
                 reply_to=reply_to,
-                attributes=attributes,
-                thumb=thumb,
-                allow_cache=allow_cache,
                 parse_mode=parse_mode,
                 formatting_entities=formatting_entities,
-                voice_note=voice_note,
-                video_note=video_note,
-                buttons=buttons,
+                link_preview=False,
                 silent=silent,
-                supports_streaming=supports_streaming,
                 schedule=schedule,
                 comment_to=comment_to,
-                **kwargs,
             )
         msglink = await client.get_msg_link(response)
         msg = f"__Sorry I can't send this message in public chats it may have some sensitive data So check in __[Bot log group]({msglink})."
