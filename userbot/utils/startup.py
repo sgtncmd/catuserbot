b8761327f021adebe5179ce67a24a82f4bd7a83a
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-27 13:45:42 +0530

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -257,10 +257,13 @@ async def verifyLoggerGroup():
 async def checkplugins(filename):
     if filename == "userbot/core/events.py":
         return
+    print(1)
     with open(filename, "r") as f:
         filedata = f.read()
+    print(filedata)
     filedata = filedata.replace("sendmessage", "send_message")
     filedata = filedata.replace("sendfile", "send_file")
     filedata = filedata.replace("editmessage", "edit_message")
+    print(filedata)
     with open(filename, "w") as f:
         f.write(filedata)
