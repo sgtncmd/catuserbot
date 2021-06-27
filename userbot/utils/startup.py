69888514fc37c62795f82f5a36c218b90a12221d
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-27 08:12:48 +0000

    pylint: auto fixes

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -252,14 +252,15 @@ async def verifyLoggerGroup():
         args = [executable, "-m", "userbot"]
         os.execle(executable, *args, os.environ)
         sys.exit(0)
-        
+
+
 async def checkplugins(filename):
     if filename == "userbot/core/events.py":
         return
-    with open(filename, 'r') as f:
-      filedata = f.read()
-    filedata = filedata.replace('sendmessage', 'send_message')
-    filedata = filedata.replace('sendfile', 'send_file')
-    filedata = filedata.replace('editmessage', 'edit_message')
-    with open(filename, 'w') as f:
-      f.write(filedata)
+    with open(filename, "r") as f:
+        filedata = f.read()
+    filedata = filedata.replace("sendmessage", "send_message")
+    filedata = filedata.replace("sendfile", "send_file")
+    filedata = filedata.replace("editmessage", "edit_message")
+    with open(filename, "w") as f:
+        f.write(filedata)
