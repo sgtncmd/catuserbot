0e1800200fad062e71062914270acdedc86c25e8
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-27 14:08:34 +0530

    Update pluginmanager.py

--- userbot/utils/pluginmanager.py
+++ userbot/utils/pluginmanager.py
@@ -95,16 +95,16 @@ def remove_plugin(shortname):
 def checkplugins(filename):
     if filename == "userbot/core/events.py":
         return
-    if filename == "userbot/plugins/1.py":
+    if str(filename) == "userbot/plugins/1.py":
         print(1)
     with open(filename, "r") as f:
         filedata = f.read()
-    if filename == "userbot/plugins/1.py":
+    if str(filename) == "userbot/plugins/1.py":
         print(filedata)
     filedata = filedata.replace("sendmessage", "send_message")
     filedata = filedata.replace("sendfile", "send_file")
     filedata = filedata.replace("editmessage", "edit_message")
-    if filename == "userbot/plugins/1.py":
+    if str(filename) == "userbot/plugins/1.py":
         print(filedata)
     with open(filename, "w") as f:
         f.write(filedata)
