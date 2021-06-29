df9696f24c7593f560aeced1d0602bfb0c20efc1
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-29 22:50:09 +0530

    Update findquote.py

--- userbot/helpers/functions/findquote.py
+++ userbot/helpers/functions/findquote.py
@@ -14,15 +14,15 @@ async def extract_quote(url):
     return results
 
 
-async def random_quote(self):
+async def random_quote():
     pgno = random.randint(1, 100)
     quoteurl = f"https://www.goodreads.com/quotes?format=html&mobile_xhr=1&page={pgno}"
     results = await extract_quote(quoteurl)
     return random.choice(results)
 
 
-async def search_quotes(search_query, pages=1):
-    random.randint(1, 5)
-    quoteurl = f"https://www.goodreads.com/quotes/search?commit=Search&page={page}&q={search_query.replace(' ', '+')}&utf8=%E2%9C%93"
+async def search_quotes(query):
+    page = random.randint(1, 5)
+    quoteurl = f"https://www.goodreads.com/quotes/search?commit=Search&page={page}&q={query.replace(' ', '+')}&utf8=%E2%9C%93"
     results = await extract_quote(quoteurl)
     return random.choice(results)
